da9868ae2870c347790afe4e56931f638bb9807f
Animay Prakash <20CS3073@rgipt.ac.in>
2023-06-20 16:21:14 +0530

    Added solution - LeetHub

---
+++ Identical Linked Lists - GFG/identical-linked-lists.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+bool areIdentical(struct Node *a, struct Node *b);
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int n1, n2, tmp , d1 , d2;
+        struct Node *head1 = NULL , *tail1=NULL;
+        struct Node *head2 = NULL , *tail2 =NULL;
+        cin>>n1;
+        cin>>d1;
+        head1 = new Node(d1);
+        tail1 = head1;
+        while(n1-- > 1){
+            cin>>tmp;
+            tail1->next = new Node(tmp);
+            tail1 = tail1->next;
+        }
+        cin>>n2;
+        cin>>d2;
+        head2 = new Node(d2);
+        tail2 = head2;
+        while(n2-- >1)
+        {
+            cin>>tmp;
+            tail2->next = new Node(tmp);
+            tail2 = tail2->next;
+        }
+        areIdentical(head1, head2)?cout<<"Identical"<<endl:cout<<"Not identical"<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to check whether two linked lists are identical or not. 
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    // Code here
+    vector<int> a,b;
+    while(head1!=NULL){
+        a.push_back(head1->data);
+        head1=head1->next;
+    }
+    while(head2!=NULL){
+        b.push_back(head2->data);
+        head2=head2->next;
+    }
+    if(a==b){
+        return true;
+    }
+    return false;
+}
